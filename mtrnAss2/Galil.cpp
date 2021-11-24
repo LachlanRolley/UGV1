28208f13f5d2301432a173ec79ac7bbbcec791d9
Lachlan Rolley <z5162440@ad.unsw.edu.au>
2021-11-24 16:26:48 +1100

    Add project files.

---
+++ mtrnAss2/Galil.cpp
@@ -0,0 +1,15 @@
+#include "Galil.h";
+#include <stdint.h>
+#include<stdio.h>
+#include "EmbeddedFunctions.h"
+#include <string>
+#include <iostream>
+#include <chrono>
+#include <Windows.h>
+
+
+
+//do them all like this pre much
+void Galil::DigitalOutput(uint16_t a) {
+	//int i = i;
+}
