ee48d9e4ef14d303e0a606188659785d1f05a2bb
Lachlan Rolley <z5162440@ad.unsw.edu.au>
2021-11-24 16:45:25 +1100

    put in the scaffolding for Galil.cpp

--- mtrnAss2/Galil.cpp
+++ mtrnAss2/Galil.cpp
@@ -9,7 +9,70 @@
 
 
 
-//do them all like this pre much
+
+
+
+											
+// DIGITAL OUTPUTS
+						// Write to all 16 bits of digital output, 1 command to the Galil
 void Galil::DigitalOutput(uint16_t a) {
-	//int i = i;
+
+}
+void Galil::DigitalByteOutput(bool bank, uint8_t value) {// Write to one byte, either high or low byte, as specified by user in 'bank'
+	
+}												// 0 = low, 1 = high
+void Galil::DigitalBitOutput(bool val, uint8_t bit) {			// Write single bit to digital outputs. 'bit' specifies which bit
+
+}
+
+// DIGITAL INPUTS
+uint16_t Galil::DigitalInput() {				// Return the 16 bits of input data
+
+}											// Query the digital inputs of the GALIL, See Galil command library @IN
+uint8_t Galil::DigitalByteInput(bool bank) {	// Read either high or low byte, as specified by user in 'bank'
+										// 0 = low, 1 = high
+}
+bool Galil::DigitalBitInput(uint8_t bit) {		// Read single bit from current digital inputs. Above functions
+										// may use this function
+}
+bool Galil::CheckSuccessfulWrite() {	// Check the string response from the Galil to check that the last 
+								// command executed correctly. 1 = succesful. NOT AUTOMARKED
+}
+
+// ANALOG FUNCITONS
+float Galil::AnalogInput(uint8_t channel) {						// Read Analog channel and return voltage	
+}
+void Galil::AnalogOutput(uint8_t channel, double voltage) {		// Write to any channel of the Galil, send voltages as
+														// 2 decimal place in the command string
+}
+void AnalogInputRange(uint8_t channel, uint8_t range) {	// Configure the range of the input channel with
+														// the desired range code
+}
+
+// ENCODER / CONTROL FUNCTIONS
+void Galil::WriteEncoder() {								// Manually Set the encoder value to zero
+}
+int Galil::ReadEncoder() {									// Read from Encoder
+}
+void Galil::setSetPoint(int s) {							// Set the desired setpoint for control loops, counts or counts/sec
+}
+void Galil::setKp(double gain) {							// Set the proportional gain of the controller used in controlLoop()
+}
+void Galil::setKi(double gain) {							// Set the integral gain of the controller used in controlLoop()
+}
+void Galil::setKd(double gain) {							// Set the derivative gain of the controller used in controlLoop()
+}
+void Galil::PositionControl(bool debug, int Motorchannel) {	// Run the control loop. ReadEncoder() is the input to the loop. The motor is the output.
+													// The loop will run using the PID values specified in the data of this object, and has an 
+													// automatic timeout of 10s. You do not need to implement this function, it is defined in
+													// GalilControl.lib
+}
+void Galil::SpeedControl(bool debug, int Motorchannel) {	// same as above. Setpoint interpreted as counts per second
+}
+
+
+// Operator overload for '<<' operator. So the user can say cout << Galil; This function should print out the
+// output of GInfo and GVersion, with two newLines after each.
+std::ostream& operator<<(std::ostream& output, Galil& galil) {
+
 }
--- mtrnAss2/Source.cpp
+++ mtrnAss2/Source.cpp
@@ -1,8 +1,21 @@
+#include <stdlib.h>
+#include <Galil.h>
+#include <gclibo.h>
+#include <gclib_errors.h>
+#include <gclib_record.h>
+#include <iostream>
+#include <fstream>
+#include <string>
 
+using namespace System;
+using namespace System::IO;
+using namespace System::Collections::Generic;
+using namespace System::Threading::Tasks;
 
 
 
 int main(void) {
+	EmbeddedFunctions* funcs = new EmbeddedFunctions();
 
 	return 0;
 }
